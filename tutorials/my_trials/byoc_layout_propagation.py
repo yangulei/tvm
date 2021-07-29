b9f1ef67e136c5cf8f4e1d9af7c04423441fd64d
crazydemo <452642781@qq.com>
2021-07-29 08:17:45 +0800

    byoc_runtime_layout_transform_done!

--- tutorials/my_trials/byoc_layout_propagation.py
+++ tutorials/my_trials/byoc_layout_propagation.py
@@ -1,5 +1,4 @@
 # BENCHMARKING SCRIPT FOR GLUON MXNET 2.0
-<<<<<<< HEAD
 from hashlib import new
 import time
 import mxnet as mx
@@ -14,11 +13,6 @@ from tvm.relay.transform.transform import AlterOpLayout, CanonicalizeCast
 from tvm.relay.build_module import GraphExecutor
 from tvm.relay.expr import Tuple
 from tvm.relay.transform.transform import AlterOpLayout
-=======
-import time
-import mxnet as mx
-import warnings
->>>>>>> f61fd3a70... enabling layout propagation with dnnl backend first trial
 
 from tvm.relay.build_module import GraphExecutor
 from tvm.relay.transform.transform import AlterOpLayout
@@ -36,14 +30,11 @@ import os
 from tvm.contrib import utils
 from tvm.relay.testing.temp_op_attr import TempOpAttr
 from tvm.relay import transform, analysis
-<<<<<<< HEAD
 from tvm.relay.build_module import bind_params_by_name
 from tvm.contrib.download import download_testdata
 from mxnet.gluon.model_zoo.vision import get_model
 from PIL import Image
 from matplotlib import pyplot as plt
-=======
->>>>>>> f61fd3a70... enabling layout propagation with dnnl backend first trial
 
 import mxnet as mx
 from mxnet.gluon import HybridBlock, nn
@@ -56,7 +47,6 @@ class PrintIR:
         print("Running pass: {}", info)
         print(mod)
 
-<<<<<<< HEAD
 def transform_image(image):
     image = np.array(image) - np.array([123.0, 117.0, 104.0])
     image /= np.array([58.395, 57.12, 57.375])
@@ -90,8 +80,6 @@ def alter_conv2d(attrs, inputs, tinfos, out_type):
             return relay.nn.conv2d(data, weight, **new_attrs)
         return relay.nn.conv2d(data, weight, **new_attrs)
 
-=======
->>>>>>> f61fd3a70... enabling layout propagation with dnnl backend first trial
 def update_lib(lib):
     # Include the path of src/runtime/contrib/dnnl/dnnl.cc
     test_dir = os.path.dirname(os.path.realpath(os.path.expanduser(__file__)))
@@ -114,7 +102,6 @@ def update_lib(lib):
     lib = tvm.runtime.load_module(lib_path)
     return lib
 
-<<<<<<< HEAD
 @relay.transform.function_pass(opt_level=1)
 class CustomPipeline:
     """Simple test function to replace one argument to another."""
@@ -236,30 +223,10 @@ class CustomPipeline:
             return relay.nn.max_pool2d(args[0], (3, 3), (2, 2), padding=(1, 1))
         else:
             return False
-=======
-
-def alter_conv2d(attrs, inputs, tinfos, out_type):
-        data, weight = inputs
-        new_attrs = dict(attrs)
-        new_attrs["data_layout"] = "NCHW16c"
-        new_attrs["kernel_layout"] = "OIHW16i"
-        return relay.nn.conv2d(data, weight, **new_attrs)
-
-def run_opt_pass(mod, passes, params):
-    passes = passes if isinstance(passes, list) else [passes]
-    # mod = tvm.IRModule.from_expr(expr)
-    seq = tvm.transform.Sequential(passes)
-    with tvm.transform.PassContext(opt_level=3):
-        mod = seq(mod)
-        json, lib, param = tvm.relay.build(mod, target="llvm", params=params)
-    # entry = mod["main"]
-    return json, lib, param
->>>>>>> f61fd3a70... enabling layout propagation with dnnl backend first trial
 
 class Model(HybridBlock):
     def __init__(self, **kwargs):
         super(Model, self).__init__(**kwargs)
-<<<<<<< HEAD
         self.relu = nn.Activation('relu')
         self.conv0 = nn.Conv2D(64, 7, use_bias=False, strides=(2, 2), padding=(3,3))
         self.bn0 = nn.BatchNorm()
@@ -376,69 +343,3 @@ def benchmark(batch_size=1, batches=10, warmup=2, cin=3):
 
 benchmark(batch_size=1)
 
-=======
-        # use name_scope to give child Blocks appropriate names.
-        # with self.name_scope():
-        self.conv0 = nn.Conv2D(256, 3, use_bias=False)# + mx.nd.random.uniform(-1.0, 1.0, shape=(256))
-        self.conv1 = nn.Conv2D(256, 3, use_bias=False)# + mx.nd.random.uniform(-1.0, 1.0, shape=(512))
-        self.conv2 = nn.Conv2D(256, 3, use_bias=False)# + mx.nd.random.uniform(-1.0, 1.0, shape=(512))
-        self.conv3 = nn.Conv2D(256, 3, use_bias=False)
-        self.relu = nn.Activation('relu')
-
-    def hybrid_forward(self, F, x):
-        x = self.relu(self.conv0(x))
-        x1 = self.relu(self.conv1(x))
-        x2 = self.relu(self.conv2(x))
-        x3 = self.relu(self.conv3(x))
-        return x1+x2+x3
-
-def benchmark(batch_size=1, batches=10, warmup=2, cin=16):
-    mx.random.seed(0)
-    sample = mx.nd.random.uniform(-1.0, 1.0, shape=(batch_size,cin,224,224))
-    target = "llvm -model=platinum-8124m -mcpu=skylake-avx512"
-    ctx = mx.cpu()
-
-    input_shape = (batch_size, cin, 224, 224)
-    
-    model = Model()
-    model.initialize(ctx=ctx)
-    model(sample)
-
-    mod, params = relay.frontend.from_mxnet(model, shape={"data": input_shape}, dtype="float32")#port the Gluon model to a portable computational graph
-    # print(mod)
-# 
-    seq = tvm.transform.Sequential(
-        [
-            transform.ConvertLayout(
-                {
-                    "nn.conv2d": ["NCHW16c", "OIHW16o16i"],
-                    "nn.conv3d": ["NCDHW", "default"],
-                    "nn.conv2d_transpose": ["NCHW", "default"],
-                }
-            ),
-            # transform.AlterOpLayout(),
-            transform.MergeComposite(pattern_table()),
-            transform.AnnotateTarget("dnnl"),
-            transform.MergeCompilerRegions(),
-            transform.PartitionGraph(),
-            
-        ]
-    )
-
-    with tvm.transform.PassContext(opt_level=3):#compile the graph , instruments=[PrintIR()]
-        graph, lib, param = tvm.relay.build(seq(mod), target="llvm", params=params)
-    # lib = update_lib(lib)
-    rt_mod = tvm.contrib.graph_executor.create(graph, lib, tvm.cpu())#Create a runtime executor module given a graph and module.
-
-    # data = np.random.uniform(size=input_shape)
-    # # rt_mod.set_input("data", sample)
-    # rt_mod.set_input("data", tvm.nd.array(data.astype("float32")))
-    # for i in range(batches+warmup):
-    #     if i == warmup:
-    #         tic = time.time()
-    #     out = rt_mod.run()
-    # with_fuse_ms = (time.time() - tic) / (batches) * 1000
-    # print("{}: with_fuse_ms: {:.4f} ms".format("net_with_branches", with_fuse_ms))
-
-benchmark(batch_size=1) 
->>>>>>> f61fd3a70... enabling layout propagation with dnnl backend first trial
