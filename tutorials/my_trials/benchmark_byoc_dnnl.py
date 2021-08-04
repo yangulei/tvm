4258f80d8e3e5bda8e70609b2ae3e2071864f39c
crazydemo <452642781@qq.com>
2021-08-04 08:27:06 +0800

    enable resnet, cancel the registered conv_bias pattern, has validate the correctness.

--- tutorials/my_trials/benchmark_byoc_dnnl.py
+++ tutorials/my_trials/benchmark_byoc_dnnl.py
@@ -18,10 +18,13 @@ from tvm.relay.testing import *
 import os
 from tvm.contrib import utils
 from tvm.relay.build_module import bind_params_by_name
+from tvm.contrib.download import download_testdata
+from mxnet.gluon.model_zoo.vision import get_model
+from PIL import Image
+from matplotlib import pyplot as plt
 
 # model_dict = {'resnet50_v1': resnet50_v1}#{'mobilenet_v2_1_0': mobilenet_v2_1_0}
 model_dict = {'resnet50_v1': resnet}
-# model_dict = {'resnet50_v1': resnet, 'mobilenet_v2_1_0': mobilenet}
 
 @tvm.instrument.pass_instrument
 class PrintIR:
@@ -31,23 +34,20 @@ class PrintIR:
         print("Running pass: {}", info)
         print(mod)
 
-@relay.op.register_alter_op_layout("nn.conv2d", level=114)
+@relay.op.register_alter_op_layout("nn.conv2d", level=400)
 def alter_conv2d(attrs, inputs, tinfos, out_type):
-    data_layout = attrs['data_layout']
-    kernel_layout = attrs['kernel_layout']
     data, weight = inputs
     new_attrs = dict(attrs)
     new_attrs['data_layout'] = 'NCHW'
     new_attrs['kernel_layout'] = 'OIHW16o'
     try:
-        if weight.type_annotation.shape[1]>=8:
-            new_attrs = dict(attrs)
+        # if weight.type_annotation.shape[1]>=16:
+        if weight.data.shape[1]>=16:
             new_attrs['data_layout'] = 'NCHW16c'
             new_attrs['kernel_layout'] = 'OIHW16o16i'
             return relay.nn.conv2d(data, weight, **new_attrs)
     except:
         return relay.nn.conv2d(data, weight, **new_attrs)
-    return relay.nn.conv2d(data, weight, **new_attrs)
 
 def update_lib(lib):
     # Include the path of src/runtime/contrib/dnnl/dnnl.cc
@@ -71,58 +71,61 @@ def update_lib(lib):
     lib = tvm.runtime.load_module(lib_path)
     return lib
 
+def transform_image(image):
+    image = np.array(image) - np.array([123.0, 117.0, 104.0])
+    image /= np.array([58.395, 57.12, 57.375])
+    image = image.transpose((2, 0, 1))
+    image = image[np.newaxis, :]
+    return image
 
 def benchmark(batch_size=1, batches=10, warmup=2):
-    mx.random.seed(0)
-    sample = mx.nd.random.uniform(-1.0, 1.0, shape=(batch_size,3,224,224))
+    img_url = "https://github.com/dmlc/mxnet.js/blob/main/data/cat.png?raw=true"
+    img_name = "cat.png"
+    img_path = download_testdata(img_url, img_name, module="data")
+    image = Image.open(img_path).resize((224, 224))
+
+    sample = transform_image(image)
+    print("x", sample.shape)
     target = "llvm -model=platinum-8124m -mcpu=skylake-avx512"
     ctx = tvm.cpu()
 
     input_shape = (batch_size, 3, 224, 224)
     for model_name in model_dict.keys():
         mod, params = model_dict[model_name].get_workload(batch_size=batch_size, dtype="float32")
-        desired_layouts = {"nn.batch_norm": ["NCHW16c", "OIHW16o16i"]}#"nn.conv2d": ["NCHW8c", "OIHW8o8i"],
+        desired_layouts = {"nn.conv2d": ["NCHW16c", "OIHW16o16i"],"nn.batch_norm": ["NCHW16c", "OIHW16o16i"]}#
         seq = tvm.transform.Sequential(
             [
-                # transform.InferType(),
-                # relay.transform.RemoveUnusedFunctions(),
                 relay.transform.CanonicalizeOps(),
                 relay.transform.SimplifyInference(),
                 relay.transform.FoldScaleAxis(),
 
                 relay.transform.AlterOpLayout(),
                 # relay.transform.ConvertLayout(desired_layouts),
-                # relay.transform.FoldConstant(),
                 relay.transform.MergeComposite(pattern_table()),
                 relay.transform.AnnotateTarget("dnnl"),
                 relay.transform.MergeCompilerRegions(),
                 relay.transform.PartitionGraph(),
-                # transform.InferType(),
             ]
         )
-        # print(seq(mod))
+
+
         if params:
             mod["main"] = bind_params_by_name(mod["main"], params)
         with tvm.transform.PassContext(opt_level=3):#, instruments=[PrintIR()]):# 
             json, lib, params = relay.build(seq(mod), "llvm", params=params)
-            # with tvm.target.Target("llvm"):
-            #     mod = seq(mod)
+        lib = update_lib(lib)
 
-        # with tvm.transform.PassContext(opt_level=3):#compile the graph , instruments=[PrintIR()]
-        #     json, lib, param = tvm.relay.build(mod, target="llvm", params=params)
-        # with relay.build_config(opt_level=3):
-        #     json, lib, params = relay.build(mod, "llvm", params=params)
-        # lib = update_lib(lib)
         rt_mod = tvm.contrib.graph_executor.create(json, lib, ctx)#Create a runtime executor module given a graph and module.
-
-        data = np.random.uniform(size=input_shape)
-        # rt_mod.set_input("data", sample)
-        rt_mod.set_input("data", tvm.nd.array(data.astype("float32")))
-        for i in range(batches+warmup):
-            if i == warmup:
-                tic = time.time()
-            out = rt_mod.run()
-        with_fuse_ms = (time.time() - tic) / (batches) * 1000
-        print("{}: with_fuse_ms: {:.4f} ms".format(model_name, with_fuse_ms))
+        
+        rt_mod.set_input("data", tvm.nd.array(sample.astype("float32")), **params)
+        rt_mod.run()
+        tvm_output = rt_mod.get_output(0)
+        print("tvm output:{}".format(tvm_output))
+        # for i in range(batches+warmup):
+        #     if i == warmup:
+        #         tic = time.time()
+        #     out = rt_mod.run()
+        # with_fuse_ms = (time.time() - tic) / (batches) * 1000
+        # print("{}: with_fuse_ms: {:.4f} ms".format(model_name, with_fuse_ms))
 
 benchmark(batch_size=1)
