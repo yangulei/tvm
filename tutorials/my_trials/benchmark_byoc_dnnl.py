d893351745294713ecbfe88ed5223854787fe35e
crazydemo <452642781@qq.com>
2021-08-10 04:33:51 +0800

    solve resnet alterlayout precision

--- tutorials/my_trials/benchmark_byoc_dnnl.py
+++ tutorials/my_trials/benchmark_byoc_dnnl.py
@@ -68,6 +68,7 @@ from tvm.contrib.download import download_testdata
 from mxnet.gluon.model_zoo.vision import get_model
 from PIL import Image
 from matplotlib import pyplot as plt
+from tvm.contrib.debugger import debug_executor as graph_executor
 
 model_dict = {'resnet50_v1': resnet}
 
@@ -249,6 +250,25 @@ def alter_conv2d(attrs, inputs, tinfos, out_type):
 
     res = relay.query_layout.AutoQuery(N,IC,KH,KW,OC,SH,SW,PH_L,PH_R,PW_L,PW_R,OH,OW)
     new_attrs = dict(attrs)
+<<<<<<< HEAD
+=======
+    new_attrs['data_layout'] = 'NCHW'
+    new_attrs['kernel_layout'] = 'OIHW'#'OHWI8o'
+    try:
+        if weight.type_annotation.shape[1]>=8:
+            new_attrs = dict(attrs)
+            new_attrs['data_layout'] = 'NCHW8c'
+            new_attrs['kernel_layout'] = 'OIHW8i8o'#'OIHW'
+            return relay.nn.conv2d(data, weight, **new_attrs)
+    except:
+        if weight.data.shape[1]>=8:
+            new_attrs = dict(attrs)
+            new_attrs['data_layout'] = 'NCHW8c'
+            new_attrs['kernel_layout'] = 'OIHW8i8o'#'OIHW'
+            return relay.nn.conv2d(data, weight, **new_attrs)
+        return relay.nn.conv2d(data, weight, **new_attrs)
+    return relay.nn.conv2d(data, weight, **new_attrs)
+>>>>>>> 0c5a2c743... solve resnet alterlayout precision
 
     src_df, weight_df, dst_df = res.split(',')
 
@@ -279,7 +299,21 @@ def transform_image(image):
     image = image[np.newaxis, :]
     return image
 
+<<<<<<< HEAD
 def benchmark(network, batch_size, profiling=False, check_acc=False, warmup=100, batches=400, dtype="float32", target="llvm"):
+=======
+def benchmark(batch_size=1, batches=10, warmup=2):
+    img_url = "https://github.com/dmlc/mxnet.js/blob/main/data/cat.png?raw=true"
+    img_name = "cat.png"
+    img_path = download_testdata(img_url, img_name, module="data")
+    image = Image.open(img_path).resize((224, 224))
+    sample = transform_image(image)
+    # print("x", sample.shape)
+    # np.random.seed(0)
+    # sample = np.ones((batch_size, 3, 224, 224))#np.random.rand(batch_size, 3, 224, 224)
+
+    target = "llvm -model=platinum-8124m -mcpu=skylake-avx512"
+>>>>>>> 0c5a2c743... solve resnet alterlayout precision
     ctx = tvm.cpu()
     input_shape = (batch_size, 3, 224, 224)
     if network=="InceptionV3":
@@ -339,7 +373,57 @@ def benchmark(network, batch_size, profiling=False, check_acc=False, warmup=100,
         rt_mod.set_input(**params)
         out = rt_mod.run()
         sample_for_mxnet = mx.ndarray.array(sample)
+<<<<<<< HEAD
         mxnet_output = block(sample_for_mxnet)
+=======
+        output = block(sample_for_mxnet)
+        print("mxnet output:{}".format(output))
+        # print(params)
+        desired_layouts = {"nn.conv2d": ["NCHW16c", "OIHW16o16i"],"nn.batch_norm": ["NCHW16c", "OIHW16o16i"]}#
+        seq = tvm.transform.Sequential(
+            [
+                # relay.transform.CanonicalizeOps(),
+                # relay.transform.SimplifyInference(),
+                # relay.transform.FoldScaleAxis(),
+                # relay.transform.SimplifyExpr(),
+                # relay.transform.FuseOps(),
+                # tvm.transform.PrintIR(),
+                relay.transform.AlterOpLayout(),
+                # tvm.transform.PrintIR(),
+                # relay.transform.ConvertLayout(desired_layouts),
+                # relay.transform.MergeComposite(pattern_table()),
+                relay.transform.AnnotateTarget("dnnl"),
+                relay.transform.MergeCompilerRegions(),
+                relay.transform.PartitionGraph(),
+                # tvm.transform.PrintIR(),
+            ]
+        )
+
+
+        # if params:
+        #     mod["main"] = bind_params_by_name(mod["main"], params)
+        with tvm.transform.PassContext(opt_level=3):#, instruments=[PrintIR()]):# 
+            json, lib, params = relay.build(seq(mod), "llvm", params=params)
+        lib = update_lib(lib)
+        # print(json)
+        rt_mod = graph_executor.create(json, lib, ctx, dump_root="/home/zy/tvm/tutorials/experiment_res/")#Create a runtime executor module given a graph and module.
+        
+        rt_mod.set_input("data", tvm.nd.array(sample.astype("float32")))
+        rt_mod.set_input(**params)
+        rt_mod.run()
+
+        # out= rt_mod.debug_get_output("tvmgen_default_dnnl_0", out=tvm.nd.empty((1, 64, 112, 112), dtype="float32"))
+        # print(out)
+        # tvm_out = rt_mod.get_output(1, tvm.nd.empty((1, 1000), "float32")).numpy()
+        # print(tvm_out)
+        # for i in range(batches+warmup):
+        #     if i == warmup:
+        #         tic = time.time()
+        #     out = rt_mod.run()
+        #     # out.wait_to_read()
+        # with_fuse_fps = batches * batch_size / (time.time() - tic)
+        # print("{}: with_fuse_ms: {:.4f} ms".format(model_name, with_fuse_fps))
+>>>>>>> 0c5a2c743... solve resnet alterlayout precision
         tvm_output = rt_mod.get_output(0)
         # print("mxnet_output:{}".format(mxnet_output))
         # print("tvm_output:{}".format(tvm_output))
