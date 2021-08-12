7b49068ee28cb135127ede3ed14654a413f57b65
crazydemo <452642781@qq.com>
2021-08-13 07:32:32 +0800

    add a new pass (work for three-layer conv)

--- tutorials/my_trials/byoc_layout_propagation.py
+++ tutorials/my_trials/byoc_layout_propagation.py
@@ -289,7 +289,7 @@ def benchmark(batch_size=1, batches=10, warmup=2, cin=3):
     model.initialize(ctx=ctx)
     sample_for_mxnet = mx.ndarray.array(sample)
     output = model(sample_for_mxnet)
-    print("mxnet output:{}".format(output))
+    # print("mxnet output:{}".format(output))
 
     mod, params = relay.frontend.from_mxnet(model, shape={"data": input_shape}, dtype="float32")#port the Gluon model to a portable computational graph
     # print(mod)
