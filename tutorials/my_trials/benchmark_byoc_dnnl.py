2d7a5c291ff0774ca95863ed21eea02c2af1a7bb
crazydemo <452642781@qq.com>
2021-09-16 20:13:19 +0800

    add args

--- tutorials/my_trials/benchmark_byoc_dnnl.py
+++ tutorials/my_trials/benchmark_byoc_dnnl.py
@@ -2,33 +2,28 @@
 for resnet50 and mobilenet
 # BENCHMARKING SCRIPT FOR GLUON MXNET 2.0
 '''
+import argparse
+
 import time
 import mxnet as mx
-import warnings
 import gluoncv
 
-# from torch._C import T
+import warnings
 warnings.filterwarnings("ignore")
-from mxnet.gluon.model_zoo.vision import *
 import tvm
 from tvm.relay.op.contrib.dnnl import *
 from tvm import relay
-import tvm.contrib.graph_executor as runtime
+
 import numpy as np
-from tvm.relay.testing import *
 import os
 from tvm.contrib import utils
 from tvm.relay.build_module import bind_params_by_name
 from tvm.contrib.download import download_testdata
-from mxnet.gluon.model_zoo.vision import get_model
 from PIL import Image
-from matplotlib import pyplot as plt
-import tvm.contrib.graph_executor as graph_executor
+
 #from tvm.runtime.vm import VirtualMachine
-#from tvm.contrib.debugger import debug_executor as graph_executor
-# 
-# model_dict = {'resnet50_v1': resnet50_v1}#{'mobilenet_v2_1_0': mobilenet_v2_1_0}
-model_dict = {'resnet50_v1': resnet}
+
+network_dict = {"resnet50":"ResNet50_v1b"}
 
 @tvm.instrument.pass_instrument
 class PrintIR:
@@ -83,17 +78,12 @@ class CustomPipeline:
                 new_node = self.get_op(cur_node, new_node, cur_node.args[1])
             elif cur_node.op.name=="nn.conv2d":
                 new_node = self.get_op(cur_node, new_node, cur_node.args[1], cur_node.attrs)
-                # return new_node
             elif cur_node.op.name=="nn.dense":
                 new_node = self.get_op(cur_node, new_node, cur_node.args[1])
             elif cur_node.op.name=="nn.batch_flatten":
                 new_node = self.get_op(cur_node, new_node)
             else:
                 new_node = self.get_op(cur_node, new_node)
-                # return new_node
-                # if i==4:
-                #     return new_node
-        # print(new_node.op.name)
         return new_node
 
     def merge_consecutive_add(self, node):
@@ -117,7 +107,6 @@ class CustomPipeline:
                         self.branch_lst.append(tmp_node)
                     self.branch_dict[self.cnt] = self.branch_lst
                 
-                # print(node.op.name)
                 self.op_lst.append(node)
                 node = node.args[0]
                 self.cnt += 1
@@ -127,7 +116,6 @@ class CustomPipeline:
 
     def check_consecutive_add(self, node):
         try:
-            # print("check ...")
             return node.op.name=='add' and len(node.type_args[1].shape)==3 and node.args[0].op.name=='add' and len(node.args[0].type_args[1].shape)==3
         except:
             return False
@@ -192,28 +180,6 @@ def alter_conv2d(attrs, inputs, tinfos, out_type):
         return relay.nn.conv2d(data, weight, **new_attrs)
     return relay.nn.conv2d(data, weight, **new_attrs)
 
-def update_lib(lib):
-    # Include the path of src/runtime/contrib/dnnl/dnnl.cc
-    test_dir = os.path.dirname(os.path.realpath(os.path.expanduser(__file__)))
-    # source_dir = os.path.join(test_dir, "..", "..", "..")
-    # contrib_path = os.path.join(source_dir, "src", "runtime", "contrib")
-    source_dir = os.path.join(test_dir, "..", "tvm")
-    contrib_path = os.path.join(source_dir, "src", "runtime", "contrib")
-
-    # Setup the gcc flag to compile DNNL code.
-    kwargs = {}
-    kwargs["options"] = ["-O2", "-std=c++14", "-I" + contrib_path]
-    tmp_path = utils.tempdir()
-    lib_name = 'lib.so'
-    lib_path = tmp_path.relpath(lib_name)
-
-    # The generated C code with DNNL APIs is compiled to a binary lib.so.
-    lib.export_library(lib_path, fcompile=False, **kwargs)
-
-    # Load the lib.so back to a runtime module.
-    lib = tvm.runtime.load_module(lib_path)
-    return lib
-
 def transform_image(image):
     image = np.array(image) - np.array([123.0, 117.0, 104.0])
     image /= np.array([58.395, 57.12, 57.375])
@@ -221,86 +187,109 @@ def transform_image(image):
     image = image[np.newaxis, :]
     return image
 
-def benchmark(batch_size=1, batches=400, warmup=100):
+def benchmark(network, batch_size, profiling=False, warmup=100, batches=400, dtype="float32", target="llvm"):
     img_url = "https://github.com/dmlc/mxnet.js/blob/main/data/cat.png?raw=true"
     img_name = "cat.png"
     img_path = download_testdata(img_url, img_name, module="data")
     image = Image.open(img_path).resize((224, 224))
     sample = transform_image(image)
-    # print("x", sample.shape)
-    # np.random.seed(0)
     #sample = np.random.rand(batch_size, 3, 224, 224)#np.ones((batch_size, 3, 224, 224))#
 
-    target = "llvm -model=platinum-8124m -mcpu=skylake-avx512"
     ctx = tvm.cpu()
 
     input_shape = (batch_size, 3, 224, 224)
-    for model_name in model_dict.keys():
-        block = gluoncv.model_zoo.get_model('ResNet50_v1b', pretrained=True)
-        mod, params = relay.frontend.from_mxnet(
-            block, shape={"data": input_shape}, dtype="float32"
-        )
-        # mod, params = model_dict[model_name].get_workload(batch_size=batch_size, dtype="float32")
-        # print(mod)
-        #sample_for_mxnet = mx.ndarray.array(sample)
-        #output = block(sample_for_mxnet)
-        #print("mxnet output:{}".format(output))
-        # print(params)
-        # desired_layouts = {"nn.conv2d": ["NCHW16c", "OIHW16o16i"],"nn.batch_norm": ["NCHW16c", "OIHW16o16i"]}#
-        seq = tvm.transform.Sequential(
-            [ 
-               relay.transform.CanonicalizeOps(),
-                relay.transform.InferType(),
-                relay.transform.SimplifyInference(),
-                relay.transform.FoldConstant(),
-                relay.transform.FoldScaleAxis(),
+    block = gluoncv.model_zoo.get_model(network_dict[network], pretrained=True)
+    mod, params = relay.frontend.from_mxnet(
+        block, shape={"data": input_shape}, dtype=dtype
+    )
+    # print(mod)
+    #sample_for_mxnet = mx.ndarray.array(sample)
+    #output = block(sample_for_mxnet)
+    #print("mxnet output:{}".format(output))
 
-                CustomPipeline(),
-                relay.transform.FoldConstant(),
-                
-                relay.transform.AlterOpLayout(),
-                
-                relay.transform.MergeComposite(pattern_table()),
-                relay.transform.AnnotateTarget("dnnl"),
-                relay.transform.MergeCompilerRegions(),
-                relay.transform.PartitionGraph(),
-            ]
-        )
+    seq = tvm.transform.Sequential(
+        [ 
+            relay.transform.CanonicalizeOps(),
+            relay.transform.InferType(),
+            relay.transform.SimplifyInference(),
+            relay.transform.FoldConstant(),
+            relay.transform.FoldScaleAxis(),
+
+            CustomPipeline(),
+            relay.transform.FoldConstant(),
+            
+            relay.transform.AlterOpLayout(),
+            
+            relay.transform.MergeComposite(pattern_table()),
+            relay.transform.AnnotateTarget("dnnl"),
+            relay.transform.MergeCompilerRegions(),
+            relay.transform.PartitionGraph(),
+        ]
+    )
 
 
-        if params:
-            mod["main"] = bind_params_by_name(mod["main"], params)
-        with tvm.transform.PassContext(opt_level=3):#, instruments=[PrintIR()]):# 
-            json, lib, params = relay.build(seq(mod), "llvm", params=params)
-        #exe =  relay.vm.compile(mod, target="llvm", params=params)
-        #lib = update_lib(lib)
-        # print(json)
-        rt_mod = graph_executor.create(json, lib, ctx)#, dump_root="/home/zy/tvm/tutorials/experiment_res/")#Create a runtime executor module given a graph and module.
-        
-        rt_mod.set_input("data", tvm.nd.array(sample.astype("float32")))
-        rt_mod.set_input(**params)
-        rt_mod.run()
-        
+    if params:
+        mod["main"] = bind_params_by_name(mod["main"], params)
+    with tvm.transform.PassContext(opt_level=3):#, instruments=[PrintIR()]):# 
+        json, lib, params = relay.build(seq(mod), target=target, params=params)
+    #exe =  relay.vm.compile(mod, target="llvm", params=params)
+    if profiling:
+        from tvm.contrib.debugger import debug_executor as graph_executor
+    else:
+        import tvm.contrib.graph_executor as graph_executor
+    rt_mod = graph_executor.create(json, lib, ctx)#, dump_root="/home/zy/tvm/tutorials/experiment_res/")#Create a runtime executor module given a graph and module.
+    
+    rt_mod.set_input("data", tvm.nd.array(sample.astype("float32")))
+    rt_mod.set_input(**params)
+    rt_mod.run()
+    
+    # out= rt_mod.debug_get_output("tvmgen_default_dnnl_0", out=tvm.nd.empty((1, 64, 112, 112), dtype="float32"))
+    #vm = VirtualMachine(exe, ctx)
+    #input_list = [tvm.nd.array(sample.astype("float32"))]
+    #results = vm.invoke("main", input_list)# print(out)
+    # tvm_out = rt_mod.get_output(1, tvm.nd.empty((1, 1000), "float32")).numpy()
+    # print(tvm_out)
+    
+    for i in range(batches+warmup):
+        if i == warmup:
+            tic = time.time()
+        out = rt_mod.run()
+        #rt_mod.profile()
+        #vm.invoke("main", input_list)
+        # out.wait_to_read()
+    with_fuse_fps = batches * batch_size / (time.time() - tic)
+    print("{}: with_fuse_fps: {:.4f} fps".format(network, with_fuse_fps))
+    #tvm_output = rt_mod.get_output(0)
+    #print(tvm_output)
+    
+    #ftimer = rt_mod.module.time_evaluator("run", ctx, min_repeat_ms=500, repeat=10)
+    #print(np.array(ftimer().results))
 
-        # out= rt_mod.debug_get_output("tvmgen_default_dnnl_0", out=tvm.nd.empty((1, 64, 112, 112), dtype="float32"))
-        #vm = VirtualMachine(exe, ctx)
-        #input_list = [tvm.nd.array(sample.astype("float32"))]
-        #results = vm.invoke("main", input_list)# print(out)
-        # tvm_out = rt_mod.get_output(1, tvm.nd.empty((1, 1000), "float32")).numpy()
-        # print(tvm_out)
-        
-        for i in range(batches+warmup):
-            if i == warmup:
-                tic = time.time()
-            out = rt_mod.run()
-            #rt_mod.profile()
-            #vm.invoke("main", input_list)
-            # out.wait_to_read()
-        with_fuse_fps = batches * batch_size / (time.time() - tic)
-        print("{}: with_fuse_fps: {:.4f} fps".format(model_name, with_fuse_fps))
-        #tvm_output = rt_mod.get_output(0)
-        #print(tvm_output)
-        
-        #ftimer = rt_mod.module.time_evaluator("run", ctx, min_repeat_ms=500, repeat=10)
-        #print(np.array(ftimer().results))
-benchmark(batch_size=1)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--network",
+        type=str,
+        choices=["resnet50"],
+        default="resnet50",
+        help="The name of the neural network.",
+    )
+    parser.add_argument("--batch-size", type=int, default=1, help="The batch size")
+    parser.add_argument(
+        "--target",
+        type=str,
+        default="llvm -model=platinum-8124m -mcpu=skylake-avx512",
+        help="The compilation target.",
+    )
+    parser.add_argument("--dtype", type=str, default="float32", help="The data type.")
+    
+    parser.add_argument("--warmup", type=int, default=100)
+    parser.add_argument("--batches", type=int, default=400)
+    parser.add_argument("--profiling", type=bool, default=False)
+    args = parser.parse_args()
+
+    target = tvm.target.Target(args.target)
+
+    # Benchmark
+    benchmark(args.network, args.batch_size, profiling=args.profiling,\
+     warmup=args.warmup, batches=args.batches, dtype=args.dtype, target=args.target)
