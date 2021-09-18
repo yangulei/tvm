bc537fbe8951010870ca8fdb3674b15727630e9d
crazydemo <452642781@qq.com>
2021-09-18 09:37:51 +0800

    profile get tvm ops

--- tutorials/my_trials/test_latency_throughput.sh
+++ tutorials/my_trials/test_latency_throughput.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export OMP_NUM_THREADS=28
 export KMP_AFFINITY=granularity=fine,noduplicates,compact,1,0
-<<<<<<< HEAD
+
 
 numactl --physcpubind=0-27 --membind=0 \
 python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py --batch-size=128
@@ -15,14 +15,6 @@ python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py --batch-size=128
 # --check_acc=True
 
 ## latency fps
-=======
-
-### check acc
-# numactl --physcpubind=0-27 --membind=0 \
-# python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py \
-# --check_acc=True
-
-## latency fps
 # numactl --physcpubind=0-27 --membind=0 \
 # python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py \
 # --batch-size=1
@@ -32,26 +24,7 @@ python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py --batch-size=128
 #python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py \
 #--batch-size=1 --profiling=True # >> $HOME/tvm/tutorials/experiment_res/0917/opt_byoc_bs1.txt
 
-### throughput fps
->>>>>>> 4893a8f1c... add test shell
-# numactl --physcpubind=0-27 --membind=0 \
-# python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py \
-# --batch-size=1
-
-### latency all ops' execution time
-#numactl --physcpubind=0-27 --membind=0 \
-#python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py \
-#--batch-size=1 --profiling=True # >> $HOME/tvm/tutorials/experiment_res/0917/opt_byoc_bs1.txt
-
-<<<<<<< HEAD
 ### throughput fps
 # numactl --physcpubind=0-27 --membind=0 \
 # python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py \
 # --batch-size=128
-
-=======
-### throughput all ops' execution time
-numactl --physcpubind=0-27 --membind=0 \
-python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py \
---batch-size=128 >> $HOME/tvm/tutorials/experiment_res/0917/opt_byoc_bs128.txt
->>>>>>> 4893a8f1c... add test shell
