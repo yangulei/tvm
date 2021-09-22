4b0210f985ffa168ed67eb0a6e762602bf76d18b
crazydemo <452642781@qq.com>
2021-09-22 18:17:53 +0800

    enable auto query in tir

--- tutorials/my_trials/benchmark_byoc_dnnl.py
+++ tutorials/my_trials/benchmark_byoc_dnnl.py
@@ -69,6 +69,14 @@ from matplotlib import pyplot as plt
 from tvm.contrib.debugger import debug_executor as graph_executor
 model_dict = {'resnet50_v1': resnet}
 
+translate_dict = {"abcd":"NCHW",
+                "Acdb8a": "OHWI8o",
+                "Acdb16a": "OHWI16o",
+                "ABcd8b8a": "OIHW8i8o",
+                "ABcd16b16a": "OIHW16i16o",
+                "aBcd8b": "NCHW8c",
+                "aBcd16b": "NCHW16c",}
+
 @tvm.instrument.pass_instrument
 class PrintIR:
     """Print the name of the pass, the IR, only before passes execute."""
