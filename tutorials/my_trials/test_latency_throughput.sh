364f6a9822adb94cfef01333022783b8ea4ac78e
crazydemo <452642781@qq.com>
2021-09-22 17:06:25 +0800

    ensure precision / fps

--- tutorials/my_trials/test_latency_throughput.sh
+++ tutorials/my_trials/test_latency_throughput.sh
@@ -1,6 +1,13 @@
+<<<<<<< HEAD
 #!/bin/bash
 export OMP_NUM_THREADS=28
 export KMP_AFFINITY=granularity=fine,noduplicates,compact,1,0
+=======
+OMP_NUM_THREADS=28 \
+KMP_AFFINITY=granularity=fine,noduplicates,compact,1,0 \
+numactl --physcpubind=0-27 --membind=0 \
+python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py --profiling=True
+>>>>>>> 8150b792b... ensure precision / fps
 
 ### check acc
 # numactl --physcpubind=0-27 --membind=0 \
@@ -22,7 +29,15 @@ export KMP_AFFINITY=granularity=fine,noduplicates,compact,1,0
 # python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py \
 # --batch-size=128
 
+<<<<<<< HEAD
 ### throughput all ops' execution time
 numactl --physcpubind=0-27 --membind=0 \
 python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py \
 --batch-size=1 #>> $HOME/tvm/tutorials/experiment_res/0917/opt_byoc_bs128_v1.7.txt
+=======
+# OMP_NUM_THREADS=28 \
+# KMP_AFFINITY=granularity=fine,noduplicates,compact,1,0 \
+# numactl --physcpubind=0-27 --membind=0 \
+# python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py \
+# --batch-size=128 --profiling=True >> $HOME/tvm/tutorials/experiment_res/0916/opt_byoc_bs128.txt
+>>>>>>> 8150b792b... ensure precision / fps
