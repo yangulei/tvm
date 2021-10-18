bed82e0099a0530f747ebe409750d8f296bb5339
crazydemo <452642781@qq.com>
2021-10-18 13:54:37 +0800

    10/18 enable models with the latest onednn (v2.4)

--- tutorials/my_trials/test_latency_throughput.sh
+++ tutorials/my_trials/test_latency_throughput.sh
@@ -3,15 +3,11 @@ export OMP_NUM_THREADS=28
 export KMP_AFFINITY=granularity=fine,noduplicates,compact,1,0
 
 numactl --physcpubind=0-27 --membind=0 \
-python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py --batch-size=128
+python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py --batch-size=1 --check_acc=True
 
-# for ((i=0; i<7; i++))
-#     do
-#         startCore=$[${i}*4]
-#         endCore=$[${startCore}+3]
-#         numactl --physcpubind=${startCore}-${endCore} --membind=0 \
-#         python /home2/zhangya9/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py& #--profiling=True& #>> /home2/zhangya9/tvm/tutorials/experiment_res/0917/opt_byoc_${i}_v1.7.txt &
-#     done
+# numactl --physcpubind=0-27 --membind=0 \
+# python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py --batch-size=128
+
 ### check acc
 # numactl --physcpubind=0-27 --membind=0 \
 # python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py \
